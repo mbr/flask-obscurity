70a0a920d4c2854212361948c757feefb73864ce
Marc Brinkmann <git@marcbrinkmann.de>
2012-06-01 18:44:42 +0200

    Updated js code.

--- flask_obscurity/static/js/uoe.js
+++ flask_obscurity/static/js/uoe.js
@@ -1,2 +1,31 @@
-function uoe(a){for(var a=a.split(",").map(function(a){return parseInt(a,10)}),b=a.slice(1,a[0]+1),g=(a.length-1-b.length)/2,d=[],c=0;c<g;++c){var e=b.length+1+2*c,f=a[e];d[f]=String.fromCharCode((256+a[e+1]-b[f%b.length])%256)}return d.join("")};
-$(document).ready(function(){$("a.oe-link").each(function(){if($(this).attr("data-oe")){var a=uoe($(this).attr("data-oe"));$(this).removeAttr("data-href");$(this).removeClass("oe-link");$(this).attr("href","mailto:"+a);$(this).text(a)}})});
+function uoe(a) {
+  for (var a = a.split(",").map(function (a) {
+    return parseInt(a, 10)
+  }), b = a.slice(1, a[0] + 1), g = (a.length - 1 - b.length) / 2, d = [], c = 0; c < g; ++c) {
+    var e = b.length + 1 + 2 * c,
+      f = a[e];
+    d[f] = String.fromCharCode((256 + a[e + 1] - b[f % b.length]) % 256)
+  }
+  return d.join("")
+};
+
+$(document).ready(function () {
+  $("a.oe-link").each(function () {
+    if ($(this).attr("data-oe")) {
+      var a = uoe($(this).attr("data-oe"));
+      $(this).removeAttr("data-oe")
+             .removeClass("oe-link")
+             .attr("href", "mailto:" + a)
+             .text(a);
+    }
+  });
+
+  $("span.oe-span").each(function () {
+    if ($(this).attr("data-oe")) {
+      var a = uoe($(this).attr("data-oe"));
+      $(this).removeAttr("data-oe")
+             .removeClass("oe-span")
+             .text(a);
+    }
+  });
+});
